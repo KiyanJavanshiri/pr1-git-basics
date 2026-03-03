48d5d8fdb646152a101e59ec7e0049479169b032
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2026-03-03 13:50:02 +0200

    feat: change location of port value

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import type { Request, Response } from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
+
+const PORT = process.env.APP_PORT || 5000;
 
 app.get("/", (req: Request, resp: Response) => {
   resp.send("Hello World!");
@@ -16,6 +20,6 @@ app.post("/product", (req: Request, resp: Response) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server is running at port 3000");
 });
