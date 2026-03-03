5b9aa6fba0fb996e4269d2ac189c9a400efd211a
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2026-03-03 13:45:58 +0200

    feat: add two endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,20 @@
 import express from "express";
+import type { Request, Response } from "express";
 
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, resp: Response) => {
+  resp.send("Hello World!");
+});
+
+app.post("/product", (req: Request, resp: Response) => {
+  resp.status(201).json({
+    success: true,
+    data: req.body,
+  });
+});
 
 app.listen(3000, () => {
   console.log("server is running at port 3000");
